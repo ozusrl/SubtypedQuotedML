a18beee42c7bfa3fc12afcb2907368d92ce0bd76
osa1 <omeragacan@gmail.com>
2013-01-22 09:39:55 +0200

    separate eval functions for popl 06 and 11 langs

--- src/eval.ml
+++ src/eval.ml
@@ -4,108 +4,154 @@ open Types
 exception StageException
 exception NotImplemented
 exception Failure of string
+exception UnsupportedExp of exp
+exception SelectException of (exp * id)
 
+module type Eval = sig
+  val eval        : value env -> exp -> value
+  val eval_staged : value env -> exp -> int -> exp
+end
 
+module EvalBase (EvalFail : Eval) : Eval = struct
 
+  (* Eval {{{ *********************************************************)
 
+  let rec eval env = function
+  | IdE id -> lookup env id
+  | ConstE c -> ConstV c
+  | EmpLstE -> ListV []
 
+  | AppE (f, p) ->
+      let f' = eval env f in
+      let p' = eval env p in
+      apply f' p'
 
+  | AbsE (Abs (arg_id, body)) ->
+      ClosV (Closure (env, arg_id, body))
 
-
-    in
-
-
-(* Eval {{{ *********************************************************)
-
-let rec eval env = function
-| IdE id -> lookup env id
-| ConstE c -> ConstV c
-| EmpLstE -> ListV []
-
-| AppE (f, p) ->
-    let f' = eval env f in
-    let p' = eval env p in
-    apply f' p'
-
-| AbsE (Abs (arg_id, body)) ->
-    ClosV (Closure (env, arg_id, body))
-
-| LetInE (Valbind (arg, exp), body) ->
-    let value = eval env exp in
-    let env' = bind_value env arg value in
-    eval env' body
-
-| FixE (id, (Abs (arg, body))) ->
-    let env' = bind_value env id UnitV in
-    let clos = ClosV (Closure (env', arg, body)) in
-    set_value env' id clos; clos
-
-| CondE [] -> UnitV
-| CondE ((g, e) :: r) -> (match eval env g with
-  | ConstV (CBool true) -> eval env e
-  | _ -> eval env (CondE r))
-
-| ValueE value -> value
-| BoxE exp    -> BoxV (eval_staged env exp 1)
-| UnboxE (BoxE exp) -> eval env (eval_staged env exp 1)
-| UnboxE not_box -> raise (Failure "unboxing a non-box value")
-| RunE exp -> (match eval env exp with
-  | BoxV code -> eval env code
-  | not_code -> raise (TypeMismatch ("CodeTy", val_type not_code)))
-| LiftE exp -> BoxV (ValueE (eval env exp))
-
-(* Eval }}} *********************************************************)
-
-(* Staged computations {{{ *****************************************)
-
-and eval_staged env exp n = (match exp with
-| IdE id -> IdE id
-| ConstE e -> ConstE e
-| EmpLstE -> EmpLstE
-| AppE (f, p) -> AppE (eval_staged env f n, eval_staged env p n)
-| AbsE (Abs (id, body)) -> AbsE (Abs (id, eval_staged env body n))
-| LetInE (Valbind (id, exp), body) ->
-    LetInE (Valbind (id, eval_staged env exp n), eval_staged env body n)
-| FixE (id, (Abs (arg, body))) ->
-    FixE (id, (Abs (arg, eval_staged env body n)))
-
-| CondE [] -> CondE []
-| CondE ((g, b) :: r) ->
-    let g' = eval_staged env g n in
-    let b' = eval_staged env b n in
-    let (CondE cond_rest) = eval_staged env (CondE r) n in
-    CondE ((g', b') :: cond_rest)
-
-| ValueE exp -> ValueE exp
-| BoxE exp -> BoxE (eval_staged env exp (n+1))
-| UnboxE exp ->
-    if n < 1 then raise StageException
-    else begin if n = 1 then
-      match eval env exp with
-      | BoxV exp' -> exp'
-      | ConstV c -> ConstE c
-      | _ -> raise StageException
-    else
-      UnboxE (eval_staged env exp (n-1))
-    end
-
-| RunE exp -> RunE (eval_staged env exp n)
-| LiftE exp -> LiftE (eval_staged env exp n))
-
-(* Staged computations }}} *****************************************)
-
-(* Function application {{{ *****************************************)
-
-and apply f arg =
-  let apply_stdfun stdfun arg = match stdfun with
-  | StdCurry (id, fn) -> ClosV (StdFun (fn arg))
-  | StdFunction (id, fn) -> fn arg
-  in
-  match f with
-  | ClosV (StdFun stdfun) -> apply_stdfun stdfun arg
-  | ClosV (Closure (env, id, body)) ->
-      let env' = bind_value env id arg in
+  | LetInE (Valbind (arg, exp), body) ->
+      let value = eval env exp in
+      let env' = bind_value env arg value in
       eval env' body
-  | not_clos -> raise (TypeMismatch ("FunTy", val_type not_clos))
 
-(* Function application }}} *****************************************)
+  | FixE (id, (Abs (arg, body))) ->
+      let env' = bind_value env id UnitV in
+      let clos = ClosV (Closure (env', arg, body)) in
+      set_value env' id clos; clos
+
+  | CondE [] -> UnitV
+  | CondE ((g, e) :: r) -> (match eval env g with
+    | ConstV (CBool true) -> eval env e
+    | _ -> eval env (CondE r))
+
+  | ValueE value -> value
+  | BoxE exp    -> BoxV (eval_staged env exp 1)
+  | UnboxE (BoxE exp) -> eval env (eval_staged env exp 1)
+  | UnboxE not_box -> raise (Failure "unboxing a non-box value")
+  | RunE exp -> (match eval env exp with
+    | BoxV code -> eval env code
+    | not_code -> raise (TypeMismatch ("CodeTy", val_type not_code)))
+  | LiftE exp -> BoxV (ValueE (eval env exp))
+
+  | e -> EvalFail.eval env e
+
+  (* Eval }}} *********************************************************)
+
+  (* Staged computations {{{ *****************************************)
+
+  and eval_staged env exp n = (match exp with
+  | IdE id -> IdE id
+  | ConstE e -> ConstE e
+  | EmpLstE -> EmpLstE
+  | AppE (f, p) -> AppE (eval_staged env f n, eval_staged env p n)
+  | AbsE (Abs (id, body)) -> AbsE (Abs (id, eval_staged env body n))
+  | LetInE (Valbind (id, exp), body) ->
+      LetInE (Valbind (id, eval_staged env exp n), eval_staged env body n)
+  | FixE (id, (Abs (arg, body))) ->
+      FixE (id, (Abs (arg, eval_staged env body n)))
+
+  | CondE [] -> CondE []
+  | CondE ((g, b) :: r) ->
+      let g' = eval_staged env g n in
+      let b' = eval_staged env b n in
+      let (CondE cond_rest) = eval_staged env (CondE r) n in
+      CondE ((g', b') :: cond_rest)
+
+  | ValueE exp -> ValueE exp
+  | BoxE exp -> BoxE (eval_staged env exp (n+1))
+  | UnboxE exp ->
+      if n < 1 then raise StageException
+      else begin if n = 1 then
+        match eval env exp with
+        | BoxV exp' -> exp'
+        | ConstV c -> ConstE c
+        | _ -> raise StageException
+      else
+        UnboxE (eval_staged env exp (n-1))
+      end
+
+  | RunE exp -> RunE (eval_staged env exp n)
+  | LiftE exp -> LiftE (eval_staged env exp n)
+
+  | e -> EvalFail.eval_staged env exp n)
+
+  (* Staged computations }}} ******************************************)
+
+  (* Function application {{{ *****************************************)
+
+  and apply f arg =
+    let apply_stdfun stdfun arg = match stdfun with
+    | StdCurry (id, fn) -> ClosV (StdFun (fn arg))
+    | StdFunction (id, fn) -> fn arg
+    in
+    match f with
+    | ClosV (StdFun stdfun) -> apply_stdfun stdfun arg
+    | ClosV (Closure (env, id, body)) ->
+        let env' = bind_value env id arg in
+        eval env' body
+    | not_clos -> raise (TypeMismatch ("FunTy", val_type not_clos))
+
+  (* Function application }}} *****************************************)
+
+end
+
+(* evaluator for popl 06 *)
+module Eval1 : Eval = struct
+
+  module Eval1Fail : Eval = struct
+    let eval _ exp = raise (UnsupportedExp exp)
+    let eval_staged _ exp _ = raise (UnsupportedExp exp)
+  end
+
+  module Eval = EvalBase(Eval1Fail)
+
+  let eval        = Eval.eval
+  let eval_staged = Eval.eval_staged
+
+end
+
+
+(* evaluator for popl 11 *)
+module rec Eval2 : Eval = struct
+
+  module Eval2Fail : Eval = struct
+    let eval = Eval2.eval
+    let eval_staged = Eval2.eval_staged
+  end
+
+  module Fail = EvalBase(Eval2Fail)
+
+  let rec eval env = function
+  | RecE fields ->
+      let field_vals = List.map (fun (id, e) -> (id, eval env e)) fields in
+      RecV field_vals
+  | SelectE (record, field) -> (match eval env record with
+    | RecV fields -> snd (List.find (fun (i, v) -> i = field) fields)
+    | not_rec -> raise (TypeMismatch ("RecTy", val_type not_rec)))
+
+  | exp -> Fail.eval env exp
+
+  and eval_staged env exp n =
+    failwith "staged computations are not supported in this language"
+
+end
--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,5 @@
 open Common
+open Eval
 
 let rec parse_and_eval_exprs ?(repl = false) lexbuf = 
   try
@@ -7,19 +8,20 @@ let rec parse_and_eval_exprs ?(repl = false) lexbuf =
 
     try
       (* infer type and print *)
-      let exp_ty = Types.fresh_tyvar () in
-      let subs = Types.infer Types.test_env exp exp_ty in
-      let ty = Types.apply_sub_ty subs exp_ty in
-      Printf.printf "Type: %s\n" (Types.show_type ty);
+      (*let exp_ty = Types.fresh_tyvar () in*)
+      (*let subs = Types.infer Types.test_env exp exp_ty in*)
+      (*let ty = Types.apply_sub_ty subs exp_ty in*)
+      (*Printf.printf "Type without substitution: %s\n" (Types.show_type exp_ty);*)
+      (*Printf.printf "Type: %s\n" (Types.show_type ty);*)
 
       (* eval value and print *)
-      let value = Eval.eval Eval.stdenv exp in
+      let value = Eval2.eval stdenv exp in
       Printf.printf "Return value: %s\n\n\n" (show_val value);
 
       (* run only one expression when in repl *)
       if not repl then parse_and_eval_exprs lexbuf
     with Failure s -> print_endline ("Unexpected error occured: " ^ s)
-       | Eval.TypeMismatch (expected, found) ->
+       | TypeMismatch (expected, found) ->
            Printf.printf "TypeMismatch: expected: %s, found: %s.\n" expected found
        | exc -> print_endline (Printexc.to_string exc)
 

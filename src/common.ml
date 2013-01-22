9536478ec796b1ed85abb663572de276f4a419ed
osa1 <omeragacan@gmail.com>
2013-01-22 09:38:52 +0200

    move env ops to common.ml

--- src/common.ml
+++ src/common.ml
@@ -57,3 +57,79 @@ let val_type = function
 | ClosV _ -> "TyFun"
 | BoxV _ -> "TyBox"
 | UnitV -> "TyUnit"
+exception Not_bound of id
+exception TypeMismatch of (string * string)
+
+let bind_value env id value = (id, ref value) :: env
+
+let rec lookup_ref env id = match env with
+| []             -> raise (Not_bound id)
+| ((x, v) :: xs) -> if x = id then v else lookup_ref xs id
+
+let set_value env id value =
+  let ref = lookup_ref env id in
+  ref := value
+
+let lookup env id = !(lookup_ref env id)
+
+
+(* Mappings of OCaml function and standard environment {{{ **********)
+
+let stdenv =
+  (* return a object language function from OCaml function with the type of
+   * int -> int *)
+  let mk_arith_fun id fn =
+    (* make OCaml function a curried function in object language *)
+    let inner_fun i1 = StdFunction (id, function
+      | ConstV (CInt i2) -> ConstV (CInt (fn i1 i2))
+      | not_int -> raise (TypeMismatch ("TyInt", val_type not_int)))
+    in
+    let curry_fun = StdCurry (id, function
+      | ConstV (CInt i1) -> inner_fun i1
+      | not_int -> raise (TypeMismatch ("TyInt", val_type not_int)))
+    in
+
+    ClosV (StdFun curry_fun)
+  in
+  (* map OCaml's `=` function to object language *)
+  let eq = ClosV (StdFun (StdCurry ("=", fun v1 ->
+    StdFunction ("=", fun v2 -> ConstV (CBool (v1 = v2))))))
+  in
+  (* list operations *)
+  let cons = ClosV (StdFun (StdCurry ("::", fun v1 ->
+    StdFunction ("::", function
+      | ListV lst -> ListV (v1 :: lst)
+      | not_list -> raise (TypeMismatch ("TyList", val_type not_list))))))
+  in
+
+  let head_v = ClosV (StdFun (StdFunction ("head", function
+    | ListV (h :: t) -> h
+    | ListV [] -> raise (Failure "head of empty list")
+    | not_list -> raise (TypeMismatch ("TyList", val_type not_list)))))
+  in
+
+  let tail_v = ClosV (StdFun (StdFunction ("tail", function
+    | ListV (h :: t) -> ListV t
+    | ListV [] -> raise (Failure "tail of empty list")
+    | not_list -> raise (TypeMismatch ("TyList", val_type not_list)))))
+  in
+
+  let empty_ty = ClosV (StdFun (StdFunction ("empty", function
+    | ListV [] -> ConstV (CBool true)
+    | ListV _  -> ConstV (CBool false)
+    | not_list -> raise (TypeMismatch ("TyList", val_type not_list)))))
+  in
+
+  (* standard environment *)
+  [ ("+",     ref (mk_arith_fun "+" (+)))
+  ; ("-",     ref (mk_arith_fun "-" (-)))
+  ; ("*",     ref (mk_arith_fun "*" ( * )))
+  ; ("/",     ref (mk_arith_fun "/" (/)))
+  ; ("=",     ref eq)
+  ; ("::",    ref cons)
+  ; ("head",  ref head_v)
+  ; ("tail",  ref tail_v)
+  ; ("empty", ref empty_ty)
+  ]
+
+(* Mappings of OCaml function and standard environment }}} **********)

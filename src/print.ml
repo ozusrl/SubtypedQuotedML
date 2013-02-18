3c4a77c048b2c74ddeeb60a6f9b35602256e8d62
osa1 <omeragacan@gmail.com>
2013-02-18 09:24:40 +0200

    pretty printer rewrite - WIP

--- src/print.ml
+++ src/print.ml
@@ -1,7 +1,10 @@
 open Common
 open Format
+open Types4
 
 let infix_ops = ["+"; "-"; "*"; "/"; "="; "::"]
+
+let between l r p = print_string l; p (); print_string r
 
 let rec print_exp = function
 | IdE id -> print_string id
@@ -9,199 +12,102 @@ let rec print_exp = function
 | EmpLstE -> print_string "[]"
 | AppE (e1, e2) -> (match e1, e2 with
   | AppE (IdE i, e1'), e2' when List.mem i infix_ops ->
-      open_hovbox 2;
-      print_string "(";
+      printf "@[<hov 2>(";
       print_exp e1';
-      print_space ();
-      print_string i;
-      print_space ();
+      printf "@;<1 2>%s@;<1 2>" i;
       print_exp e2';
-      print_string ")";
-      close_box ()
+      printf ")@]"
   | _ ->
-      open_hovbox 2;
-      print_string "(";
+      printf "@[<hov 2>(";
       print_exp e1;
-      print_break 1 2;
+      printf "@;<1 2>";
       print_exp e2;
-      print_string ")";
-      close_box ())
+      printf ")@]")
 | AbsE abs -> print_abs abs
 | LetInE (dec, exp) ->
-    open_hovbox 2;
-    print_string "let";
-    print_space ();
+    printf "@[<hv 2>let@ ";
     print_dec dec;
-    print_space ();
-    print_string "in";
-    print_break 1 0;
+    printf "@;<1 -2>in@;<1 -2>";
     print_exp exp;
-    close_box ()
+    printf "@]";
 | FixE (id, abs) ->
-    open_hovbox 2;
-    print_string "fix";
-    print_space ();
-    print_string id;
-    print_space ();
-    print_string "->";
-    print_break 1 2;
-    print_abs abs;
-    close_box ()
+    printf "@[<hov 2>fix@ %s@ ->@ " id; print_abs abs; printf "@]";
 | CondE conds ->
-    open_hovbox 0;
     let print_cond s (g, b) =
-      open_hovbox 2;
-      print_string s;
-      print_space ();
+      printf "@[<hov 2>%s@ " s;
       print_exp g;
-      print_space ();
-      print_string "then";
-      print_break 1 2;
+      printf "@ then@ ";
       print_exp b;
-      close_box ()
+      printf "@]";
     in
     let first_cond = List.hd conds in
     let rest_conds = List.tl conds in
-
     let rec print_conds = function
     | [] -> ()
+    | [c] -> print_cond "else" c
     | f :: r ->
         print_cond "elseif" f;
-        print_break 1 1;
+        print_space ();
         print_conds r
     in
+    open_hvbox 0;
     print_cond "if" first_cond;
     print_space ();
     print_conds rest_conds;
     close_box ()
-| RefE exp ->
-    open_hovbox 2;
-    print_string "ref";
-    print_space ();
-    print_exp exp;
-    close_box ()
-| DerefE exp ->
-    print_string "!";
-    print_exp exp
+| RefE exp -> printf "@[<hov 2>ref@ "; print_exp exp; printf "@]";
+| DerefE exp -> print_string "!"; print_exp exp
 | AssignE (e1, e2) ->
-    open_hovbox 2;
-    print_exp e1;
-    print_space ();
-    print_string ":=";
-    print_break 1 2;
-    print_exp e2;
-    close_box ();
+    printf "@[<hov 2>";
+    print_exp e1; printf "@ :=@ "; print_exp e2;
+    printf "@]";
 | ValueE v -> print_value v
-| BoxE exp ->
-    open_hovbox 2;
-    print_string "<";
-    print_exp exp;
-    print_string ">";
-    close_box ()
-| UnboxE exp ->
-    open_hovbox 2;
-    print_string "~(";
-    print_exp exp;
-    print_string ")";
-    close_box ()
-| RunE exp ->
-    open_hovbox 2;
-    print_string "run(";
-    print_exp exp;
-    print_string ")";
-    close_box ()
-| LiftE exp ->
-    open_hovbox 2;
-    print_string "lift(";
-    print_break 0 4;
-    print_exp exp;
-    print_break 0 4;
-    print_string ")";
-    close_box ()
+| BoxE exp -> printf "@[<hov 2><"; print_exp exp; printf ">@]"
+| UnboxE exp -> printf "@[<hov 2>~("; print_exp exp; printf")@]"
+| RunE exp -> printf "@[<hov 2>run("; print_exp exp; printf ")@]"
+| LiftE exp -> printf "lift(@;<0 2>"; print_exp exp; printf ")"
 | EmptyRecE -> print_string "{}"
-| SelectE (exp, field) ->
-    print_exp exp;
-    print_string ".";
-    print_string field
+| SelectE (exp, field) -> print_exp exp; printf ".%s" field
 | RecUpdE (r, id, exp) ->
-    open_hovbox 2;
-    print_string "{";
-    print_break 1 2;
+    printf "@[<hov 2>{{@ ";
     print_exp r;
-    print_break 1 2;
-    print_string ":=";
-    print_break 1 2;
+    printf ".%s@ :=@ " id;
     print_exp exp;
-    print_break 1 0;
-    print_string "}";
-    close_box ()
+    printf "@ }}@]"
 
 and print_const = function
-| CInt i -> print_int i
+| CInt i  -> print_int i
 | CBool b -> print_bool b
 
 and print_abs = function
 | Abs (id, body) ->
-    open_hovbox 2;
-    print_string "(fun";
-    print_space ();
-    print_string id;
-    print_space ();
-    print_string "->";
-    print_break 1 2;
-    print_exp body;
-    print_string ")";
-    close_box ()
+    printf "@[<hov 2>(fun@ %s@ ->@ " id; print_exp body; printf ")@]"
 
 and print_dec = function
-| Valbind (id, exp) ->
-    print_string id;
-    print_space ();
-    print_string "=";
-    print_space ();
-    print_exp exp
+| Valbind (id, exp) -> printf "@[<hov 2>%s@ =@ " id; print_exp exp; printf "@]"
 
 and print_value = function
 | ConstV c -> print_const c
-| ClosV f -> print_funval f
-| BoxV e -> print_exp (BoxE e)
+| ClosV f  -> print_funval f
+| BoxV e   -> print_exp (BoxE e)
 | ListV vals ->
-    open_hvbox 2;
-    print_string "[";
-    print_space ();
-    List.iter (fun v -> print_value v; print_space()) vals;
-    print_space ();
-    print_string "]";
-    close_box ()
+    let rec iter = function
+    | [] -> ()
+    | [v] -> print_value v;
+    | v :: vs -> print_value v; printf "@;<1 0>; "; iter vs
+    in
+    printf "@[<hov 0>[ ";
+    iter vals;
+    printf "@ ]@]"
 | RecV fields ->
     let rec print_fields = function
     | [] -> ()
-    | [(k,v)] ->
-        open_hovbox 2;
-        print_string k;
-        print_space ();
-        print_string "=";
-        print_space ();
-        print_value v;
-        close_box ();
+    | [(k,v)] -> printf "@[<hov 2>%s@ =@ " k; print_value v; printf "@]"
     | (k, v) :: rest ->
-        open_hovbox 2;
-        print_string k;
-        print_space ();
-        print_string "=";
-        print_space ();
-        print_value v;
-        print_string ",";
-        print_space ();
-        close_box ();
+        printf "@[<hov 2>%s@ =@ " k; print_value v; printf ",@ @]";
+        print_fields rest
     in
-    open_hvbox 2;
-    print_string "{";
-    print_space ();
-    print_fields fields;
-    print_space ();
-    print_string "}";
-    close_box ()
+    printf "@[<hov 2>{@ "; print_fields fields; printf "@ }@]"
 | RefV v ->
     print_string "ref";
     print_space ();
@@ -214,56 +120,71 @@ and print_funval = function
     print_string ("<stdfun " ^ id ^ " >")
 | Closure (env, id, body) -> print_abs (Abs (id, body))
 
-(*let rec print_ty = function
-| IntTy -> print_string "int"
-| BoolTy -> print_string "bool"
-| FunTy (t1, t2) ->
+let rec print_ty = function
+| TInt -> print_string "int"
+| TBool -> print_string "bool"
+| TList ty ->
+    open_hovbox 2; between "[" "]" (fun _ -> print_ty ty); close_box ();
+| TRef ty -> open_hovbox 2; print_string "ref "; print_ty ty; close_box ();
+| TFun (t1, t2) ->
     open_hovbox 2;
-    print_ty t1;
-    print_space ();
-    print_string "->";
-    print_space ();
-    print_ty t2;
-    close_box ()
-| VarTy _ as t ->
-    (match norm_ty t with
-    | VarTy v ->
-        let (tyvarkind, _) = !v in
-        (match tyvarkind with
-        | NoLink s -> print_string ("'" ^ s)
-        | _ -> failwith"")
-    | not_var -> print_ty not_var)
-| ListTy t -> print_string "["; print_ty t; print_string "]"
-| RecTy t -> print_string "{"; print_ty t; print_string "}"
-| RefTy t -> print_string "ref("; print_ty t; print_string ")"
-| EmptyRow -> print_string "{}"
-| Row r -> print_row r
+    between "(" ")"
+      (fun _ -> print_ty t1; print_space (); print_string "->";
+      print_space (); print_ty t2);
+    close_box ();
+| TRec tyrec -> print_tyrec tyrec
+| TVar typevar -> print_typevar typevar
 
-and print_row row =
-  let rec row_iter = function
-  | id, ty, nextrow ->
-      open_hovbox 2;
-      print_string id;
-      print_space ();
-      print_string "=";
-      print_space ();
-      print_ty ty;
-      (match nextrow with
-      | EmptyRow -> print_string "}"
-      | Row r ->
-          print_string ";";
-          print_space ();
-          row_iter r
-      | VarTy _ as v ->
-          print_string "|";
-          print_ty v
-      | _ -> failwith"");
-      close_box ()
+and print_tyrec = function
+| EmptyRec -> print_string "{}"
+| Rho recvar -> print_recvar recvar
+| Row (id, field, tyrec) ->
+    open_hovbox 2;
+    print_string "{";
+    printf "%s@ =@ " id;
+    print_field field;
+    print_tyrec_tail tyrec;
+    close_box ();
+
+and print_tyrec_tail = function
+| EmptyRec -> print_string "}" 
+| Rho recvar -> print_recvar_tail recvar
+| Row (id, field, tyrec) ->
+    printf ",@ %s@ =@ " id;
+    print_field field;
+    print_space ();
+    print_tyrec_tail tyrec
+
+and print_field = function
+| FieldType ty -> print_ty ty
+| FieldVar fieldvar -> print_fieldvar fieldvar
+| Bot -> printf "_|_"
+
+and print_typevar typevar = match !typevar with
+| NoLink id, _ -> printf "'%s" id
+| LinkTo ty, _ -> print_ty ty
+
+and print_recvar recvar = match !recvar with
+| NoLink id, _, btms -> printf "{'%s" id; print_idset btms; printf "}"
+| LinkTo tyrec, _, _ -> print_tyrec tyrec
+
+and print_recvar_tail recvar = match !recvar with
+| NoLink id, _, btms -> printf "|@ '%s" id; print_idset btms; printf "}"
+| LinkTo tyrec, _, _ -> print_tyrec_tail tyrec
+
+and print_idset idset =
+  let rec iter = function
+  | [] -> ()
+  | [id] -> print_string id
+  | id :: rest -> printf "%s,@ " id; iter rest
   in
+  let elems = IdSet.elements idset in
   open_hovbox 2;
-  print_string "{";
-  print_space ();
-  row_iter row;
-  print_space ();
-  print_string "}";
-  close_box ()*)
+  print_string "[";
+  iter elems;
+  print_string "]";
+  close_box ()
+
+and print_fieldvar fieldvar = match !fieldvar with
+| NoLink id, _ -> printf "'%s" id
+| LinkTo field, _ -> print_field field

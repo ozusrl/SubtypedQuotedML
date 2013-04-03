1aad8d5567b6c9cc11b0cfd70aa9256cc1f5bf40
osa1 <omeragacan@gmail.com>
2013-04-03 03:00:55 +0300

    type simplification - don't simplify if a rho var is used in somewhere else

--- wallace/toy/stagedSimplify.ml
+++ wallace/toy/stagedSimplify.ml
@@ -2,48 +2,97 @@ open StagedCommon
 open StagedTypes
 open StagedIdGen
 
-let simplify_type (ty : ty) : ty =
-  let (Scheme (vars, _)) = generalize (-1) (FieldType ty) in
-
-  let rec elim_fts_tyrec btms tyrec =
-    match norm_tyrec tyrec with
-    | EmptyRec -> failwith ""
-    | Rho recvar ->
-        (* recvar should be NoLink because of norm_tyrec call *)
-        let (link, lvl, idset) = !recvar in
-        Rho (ref (link, lvl, IdSet.diff idset btms))
-    | Row (id, FieldVar fv, rest) ->
-        (match !fv with
-        | NoLink type_id, lvl ->
-            if List.mem (FV fv) vars then
-              elim_fts_tyrec (IdSet.add id btms) rest
-            else
-              Row (id, FieldVar fv, elim_fts_tyrec btms rest)
-        | LinkTo _, _ -> failwith "unexpected case in elim_fts_tyrec: should be a bug in norm_tyrec")
-    | Row (id, f, rest) -> Row (id, f, elim_fts_tyrec btms rest)
-  in
-
-  let elim_fts_tyrec = elim_fts_tyrec IdSet.empty in
-
-  let rec elim_fts = function
-  | TInt | TBool | TUnit as ty -> ty
-  | TPair (t1, t2) -> TPair (elim_fts t1, elim_fts t2)
-  | TList ty -> TList (elim_fts ty)
-  | TRef ty -> TList (elim_fts ty)
-  | TFun (t1, t2) -> TFun (elim_fts t1, elim_fts t2)
-  | TVar typevar -> (match !typevar with
-    | NoLink _, _ -> TVar typevar
-    | LinkTo typevar, _ -> elim_fts typevar)
-  | TBox (tyrec, ty) -> TBox (elim_fts_tyrec tyrec, elim_fts ty)
-  | TRec tyrec -> TRec (elim_fts_tyrec tyrec)
-  in
-
-  elim_fts ty
-
 module IdMap = Map.Make(struct
   let compare = Pervasives.compare
   type t = string
 end)
+
+let simplify_type (ty : ty) : ty =
+  let (Scheme (vars, _)) = generalize (-1) (FieldType ty) in
+
+  let rec find_rhos_tyrec set tyrec = match norm_tyrec tyrec with
+  | EmptyRec -> set
+  | Rho recvar -> (match !recvar with
+    | NoLink id, _, _ ->
+        if IdSet.mem id set then
+          IdSet.remove id set
+        else
+          IdSet.add id set
+    | LinkTo _, _, _ -> failwith "unexpected case in find_rhos_tyrec: should be a bug in norm_tyrec")
+  | Row (_, _, rest) -> find_rhos_tyrec set rest
+  in
+
+  (* find_rhos: search in type and collect rho variables to be simplified *)
+  let rec find_rhos set = function
+  | TInt | TBool | TUnit -> set
+  | TPair (t1, t2) | TFun (t1, t2) ->
+      let set' = find_rhos set t1 in
+      find_rhos set' t2
+  | TList ty | TRef ty -> find_rhos set ty
+  | TRec tyrec -> find_rhos_tyrec set tyrec
+  | TBox (tyrec, ty) ->
+      let set' = find_rhos_tyrec set tyrec in
+      find_rhos set' ty
+  | TVar typevar -> (match !typevar with
+    | NoLink _, _ -> set
+    | LinkTo ty, _ -> find_rhos set ty)
+  in
+
+  let find_rhos = find_rhos IdSet.empty in
+
+  let rec rho_id_of_tyrec = function
+  | EmptyRec -> None
+  | Rho recvar -> (match !recvar with
+    | NoLink id, _, _ -> Some id
+    | LinkTo _, _, _ -> failwith "unexpected case in rho_id_of_tyrec: should be a bug in norm_tyrec")
+  | Row (_, _, rest) -> rho_id_of_tyrec rest
+  in
+
+  let rec elim_fts_tyrec btms rhos tyrec =
+
+    let rec iter elim = function
+    | EmptyRec -> EmptyRec
+    | Rho recvar ->
+        if elim then
+          let (link, lvl, idset) = !recvar in
+          Rho (ref (link, lvl, IdSet.diff idset btms))
+        else
+          Rho recvar
+    | Row (id, FieldVar fv, rest) ->
+        if elim then
+          match !fv with
+          | NoLink type_id, lvl ->
+              if List.mem (FV fv) vars then
+                elim_fts_tyrec (IdSet.add id btms) rhos rest
+              else
+                Row (id, FieldVar fv, elim_fts_tyrec btms rhos rest)
+          | LinkTo _, _ -> failwith "unexpected case in elim_fts_tyrec: should be a bug in norm_tyrec"
+        else
+          Row (id, FieldVar fv, elim_fts_tyrec btms rhos rest)
+    | Row (id, FieldType ty, rest) -> Row (id, FieldType (elim_fts rhos ty), rest)
+    | Row (id, f, rest) -> Row (id, f, iter elim rest)
+    in
+
+    let tyrec' = norm_tyrec tyrec in
+
+    match rho_id_of_tyrec tyrec' with
+    | None -> tyrec'
+    | Some rho -> iter (IdSet.mem rho rhos) tyrec'
+
+  and elim_fts (rhos : IdSet.t) : ty -> ty = function
+  | TInt | TBool | TUnit as ty -> ty
+  | TPair (t1, t2) -> TPair (elim_fts rhos t1, elim_fts rhos t2)
+  | TList ty -> TList (elim_fts rhos ty)
+  | TRef ty -> TList (elim_fts rhos ty)
+  | TFun (t1, t2) -> TFun (elim_fts rhos t1, elim_fts rhos t2)
+  | TVar typevar -> (match !typevar with
+    | NoLink _, _ -> TVar typevar
+    | LinkTo typevar, _ -> elim_fts rhos typevar)
+  | TBox (tyrec, ty) -> TBox (elim_fts_tyrec IdSet.empty rhos tyrec, elim_fts rhos ty)
+  | TRec tyrec -> TRec (elim_fts_tyrec IdSet.empty rhos tyrec)
+  in
+
+  elim_fts (find_rhos ty) ty
 
 module IdGen = StagedIdGen (struct end)
 open IdGen
@@ -91,5 +140,3 @@ let rename_vars (ty : ty) : ty =
   in
 
   gen_names ty
-
-
